93e62d66101cc738ac355a76b2146d5cd4be9e12
Steve Witman <stevewitman@gmail.com>
2017-08-24 23:43:29 -0600

    Add links to navbar, add add-listing route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,8 @@ import { EditListingComponent } from './components/edit-listing/edit-listing.com
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'listings', component: ListingsComponent}
+  {path: 'listings', component: ListingsComponent},
+  {path: 'add-listing', component: AddListingComponent}
 ]
 
 @NgModule({
